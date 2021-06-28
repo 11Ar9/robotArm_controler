86dd96f2883f6732e018e29d66a6b25b478febcf
11Ar9 <85986875+11Ar9@users.noreply.github.com>
2021-06-28 09:05:22 +0300

    Add files via upload

---
+++ armcon (1).sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 28, 2021 at 08:04 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `racontrol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `armcon`
+--
+
+CREATE TABLE `armcon` (
+  `m1` int(11) NOT NULL,
+  `m2` int(11) NOT NULL,
+  `m3` int(11) NOT NULL,
+  `m4` int(11) NOT NULL,
+  `m5` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
